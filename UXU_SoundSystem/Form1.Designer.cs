c3f5abf606dd91b82611b11c930ebaa445c68519
Vis <vincentvenhuizen1997@gmail.com>
2018-05-29 21:35:25 +0200

    initial commit

---
+++ UXU_SoundSystem/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace UXU_SoundSystem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.connectButton = new System.Windows.Forms.Button();
+            this.porst_comboBox = new System.Windows.Forms.ComboBox();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(162, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(16, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "0";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.connectButton);
+            this.groupBox1.Controls.Add(this.porst_comboBox);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(236, 242);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Arduino";
+            this.groupBox1.Enter += new System.EventHandler(this.groupBox1_Enter);
+            // 
+            // connectButton
+            // 
+            this.connectButton.Location = new System.Drawing.Point(6, 66);
+            this.connectButton.Name = "connectButton";
+            this.connectButton.Size = new System.Drawing.Size(201, 38);
+            this.connectButton.TabIndex = 2;
+            this.connectButton.Text = "Connect";
+            this.connectButton.UseVisualStyleBackColor = true;
+            this.connectButton.Click += new System.EventHandler(this.connectButton_Click);
+            // 
+            // porst_comboBox
+            // 
+            this.porst_comboBox.FormattingEnabled = true;
+            this.porst_comboBox.Location = new System.Drawing.Point(6, 36);
+            this.porst_comboBox.Name = "porst_comboBox";
+            this.porst_comboBox.Size = new System.Drawing.Size(201, 24);
+            this.porst_comboBox.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button2);
+            this.groupBox2.Controls.Add(this.label5);
+            this.groupBox2.Controls.Add(this.panel1);
+            this.groupBox2.Controls.Add(this.numericUpDown1);
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.label10);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Location = new System.Drawing.Point(254, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(478, 242);
+            this.groupBox2.TabIndex = 2;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Sound settings";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.DecimalPlaces = 1;
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            5,
+            0,
+            0,
+            65536});
+            this.numericUpDown1.Location = new System.Drawing.Point(366, 74);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(87, 22);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(6, 32);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(213, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Start sound receiver";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 76);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(112, 17);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Sound detected:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(124, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(38, 17);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "false";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(281, 49);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(103, 17);
+            this.label10.TabIndex = 1;
+            this.label10.Text = "Peek treshhold";
+            this.label10.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(437, 49);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(16, 17);
+            this.label9.TabIndex = 0;
+            this.label9.Text = "0";
+            this.label9.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(281, 32);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(105, 17);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Detected Peek:";
+            this.label8.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(437, 32);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(16, 17);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "0";
+            this.label7.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 93);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Detected sound value:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.panel1.Location = new System.Drawing.Point(390, 177);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(82, 59);
+            this.panel1.TabIndex = 5;
+            // 
+            // timer3
+            // 
+            this.timer3.Interval = 5000;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(6, 219);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 17);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Info";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 177);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(109, 59);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Reset old treshold";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(751, 269);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button connectButton;
+        private System.Windows.Forms.ComboBox porst_comboBox;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Timer timer3;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Button button2;
+    }
+}
+
---
+++ UXU_SoundSystem/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using CSCore.CoreAudioAPI;
+using System.IO.Ports;
+
+
+namespace UXU_SoundSystem
+{
+    public partial class Form1 : Form
+    {
+
+        bool isconnected = false;
+        bool timer1Bool = false;
+        String[] ports;
+        SerialPort port;
+
+        public Form1()
+        {
+            InitializeComponent();
+            getAvailablePorst();
+            for (int i = 0; i < ports.Length; i++)
+            {
+                Console.WriteLine(ports[i]);
+                porst_comboBox.Items.Add(ports[i]);
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+
+        private void getAvailablePorst()
+        {
+            ports = SerialPort.GetPortNames();
+        }
+        string com = null;
+        private void connect_btn_Click(object sender, EventArgs e)
+        {
+            com = porst_comboBox.GetItemText(porst_comboBox.SelectedItem).ToString();
+            if (!String.IsNullOrEmpty(com))
+            {
+                if (!isconnected)
+                {
+                    
+                    connectToArduino();
+                }
+                else
+                {
+
+                    disconnectArduino();
+
+                }
+            }
+        }
+        private void connectToArduino()
+        {
+            isconnected = true;
+
+            
+
+            timer2.Start();
+            string selectedPort = porst_comboBox.GetItemText(porst_comboBox.SelectedItem);
+            port = new SerialPort(selectedPort, 2000000, Parity.None, 8, StopBits.One);
+            try
+            {
+                port.Open();
+                port.WriteLine(" STARTEDUP");
+                connectButton.Text = "disconnect";
+            }
+            catch (Exception e)
+            {
+                port.Close();
+                Console.WriteLine(String.Format("Error ({0}) No RF module", e.Message));
+                connectButton.Text = "error";
+            }
+
+        }
+        private void disconnectArduino()
+        {
+
+            
+
+            isconnected = false;
+            connectButton.Text = "connect";
+            port.Close();
+        }
+
+
+        //SOUND SHIT!
+        public static MMDevice GetDefaultRenderDevice()
+        {
+            using (var enumerator = new MMDeviceEnumerator())
+            {
+                return enumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Console);
+            }
+        }
+        public static double IsAudioPlaying(MMDevice device)
+        {
+            using (var meter = AudioMeterInformation.FromDevice(device))
+            {
+                if (meter.PeakValue > 0.1)
+                {
+                    return meter.PeakValue;
+                } else
+                {
+                    return 0;
+                }
+            }
+        }
+
+        double piek = 0;
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            ditPiek(IsAudioPlaying(GetDefaultRenderDevice()));
+            label1.Text = ((IsAudioPlaying(GetDefaultRenderDevice()) * 100).ToString());
+            if (IsAudioPlaying(GetDefaultRenderDevice()) > 0)
+            {
+                label4.Text = "true";
+            } else
+            {
+                timer3.Start();
+            }
+        }
+        decimal old_test;
+        int i = 0;
+        bool peekUp = false;
+        private void ditPiek(double value)
+        {
+            decimal test = Convert.ToInt32(value * 100);
+            label5.Text = "Treshhold = " + old_test.ToString();
+
+
+            if (test > old_test && peekUp == false)
+            {
+                peekUp = true;
+                label7.Text = test.ToString();
+                label9.Text = (test / 100 * (100 - numericUpDown1.Value)).ToString();
+                old_test = (test / 100 * (100 - numericUpDown1.Value));
+                Console.WriteLine("Peak");
+                panel1.BackColor = Color.FromArgb(0, 255, 0);
+                if (isconnected)
+                {
+                    port.WriteLine(" p");
+                   
+                }
+            }
+            if (test < old_test / 100 * (100- numericUpDown1.Value))
+            {
+                peekUp = false;
+                Console.WriteLine("peak Low");
+                panel1.BackColor = Color.FromArgb(255, 80, 80);
+            }
+            
+
+        }
+
+        private void connectButton_Click(object sender, EventArgs e)
+        {
+            com = porst_comboBox.GetItemText(porst_comboBox.SelectedItem).ToString();
+            if (!String.IsNullOrEmpty(com))
+            {
+                if (!isconnected)
+                {
+                    connectToArduino();
+                }
+                else
+                {
+                    disconnectArduino();
+                }
+            }
+        }
+
+        private void groupBox1_Enter(object sender, EventArgs e)
+        {
+
+        }
+
+        
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Console.WriteLine(timer1Bool);
+            if (!timer1Bool)
+            {
+                Console.WriteLine(timer1Bool);
+                timer1.Start();
+                timer1Bool = true;
+                button1.Text = "Stop sound receiver";
+            }
+            else
+            {
+                label4.Text = "false";
+                label1.Text = "0";
+                timer1.Stop();
+                timer1Bool = false;
+                button1.Text = "Start sound receiver";
+            }
+            
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            old_test = 0;
+            label5.Text = "Treshhold reset";
+            peekUp = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            old_test = 0;
+            label5.Text = "Treshhold reset";
+            peekUp = false;
+        }
+    }
+}
